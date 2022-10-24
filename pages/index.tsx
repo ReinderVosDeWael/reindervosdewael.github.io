df9aa0793bec9209823b15f3d75109baa6cb9ad5
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2022-10-24 14:22:58 +0200

    Add a build test

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,35 +6,14 @@ import Meta from './components/Meta';
 import Navbar from './components/Navbar';
 import Resume from './components/Resume';
 
-import {ThemeProvider, createTheme} from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-// Say hello to the people digging under the hood :).
-console.log(
-    `%cWell... you're digging into the code of my website, eh? :-) 
-I hope you enjoy, though it may be easier to just go straight to
-the source code at:
-https://github.com/ReinderVosDeWael/ReinderVosDeWael.github.io.`,
-    'font-size: large',
-);
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 export default function Home() {
   return (
     <div className="container">
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
         <Meta />
         <Navbar />
         <Hero />
         <About />
         <Resume />
-      </ThemeProvider>
     </div>
   );
 }
