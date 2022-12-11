467ac7496914676baa1fc8d26d1badf0671ef8b9
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2022-12-11 19:48:20 +0100

    Fix mobile navbar

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 
 
 function NavBar() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="dark" variant="dark"
+    <Navbar bg="dark" variant="dark" expand="sm" expanded={expanded}
       className="fixed-nav bg-black opacity-75 text-white">
       <Container>
         <Navbar.Brand href="/#about">Reinder Vos de Wael</Navbar.Brand>
-        <Nav className="me-auto flex-grow-1 justify-content-evenly">
-          <Nav.Link href="#about"><strong>About</strong></Nav.Link>
-          <Nav.Link href="#resume"><strong>Resume</strong></Nav.Link>
-          <Nav.Link href="#contact"><strong>Contact</strong></Nav.Link>
-        </Nav>
+        <Navbar.Toggle onClick={() => setExpanded(!expanded)}/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto flex-grow-1 justify-content-evenly">
+            <Nav.Link onClick={() => setExpanded(false)}
+              href="#about"><strong>About</strong></Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)}
+              href="#resume"><strong>Resume</strong></Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)}
+              href="#contact"><strong>Contact</strong></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
