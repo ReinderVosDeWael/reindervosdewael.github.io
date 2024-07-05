58a2029ea925fefa653346ce2ca8d719ad2d4bed
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2024-07-05 17:10:53 -0400

    Run eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.css';
-import {Helmet} from "react-helmet";
+import {Helmet} from 'react-helmet';
 
 /**
  * I don't know what this does.
@@ -10,17 +10,21 @@ import {Helmet} from "react-helmet";
  * @return {Component}
  */
 function MyApp({Component, pageProps}) {
-  return <>
-    <Helmet>
-      <title>Resume</title>
-      <meta name="description" content="Resume of Reinder Vos de Wael"/>
-      <meta name="keywords" content="developer, resume"/>
-      <meta name="author" content="Reinder Vos de Wael" />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3320911161905431"
-     crossorigin="anonymous"></script>
-    </Helmet>
-    <Component {...pageProps} />;
+  return (
+    <>
+      <Helmet>
+        <title>Resume</title>
+        <meta name="description" content="Resume of Reinder Vos de Wael" />
+        <meta name="keywords" content="developer, resume" />
+        <meta name="author" content="Reinder Vos de Wael" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3320911161905431"
+          crossOrigin="anonymous"></script>
+      </Helmet>
+      <Component {...pageProps} />;
     </>
+  );
 }
 
 export default MyApp;
