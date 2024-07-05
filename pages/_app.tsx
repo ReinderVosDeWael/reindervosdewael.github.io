5a18b1a45427ca3db864c20e48bdea684e661cf6
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2024-07-05 17:05:26 -0400

    Add adsense to correct file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.css';
+import {Helmet} from "react-helmet";
 
 /**
  * I don't know what this does.
@@ -9,7 +10,17 @@ import '../styles/styles.css';
  * @return {Component}
  */
 function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />;
+  return <>
+    <Helmet>
+      <title>Resume</title>
+      <meta name="description" content="Resume of Reinder Vos de Wael"/>
+      <meta name="keywords" content="developer, resume"/>
+      <meta name="author" content="Reinder Vos de Wael" />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3320911161905431"
+     crossorigin="anonymous"></script>
+    </Helmet>
+    <Component {...pageProps} />;
+    </>
 }
 
 export default MyApp;
